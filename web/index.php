d8ddb76075316d0cb57baa00f0aea8df1aadd716
Svel <Svel.Sontz@gmail.com>
2011-03-05 17:36:47 +0300

    Шаг 4: модифицируем контроллер index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,32 @@
+<?php
+    $projectDir = dirname(dirname(__FILE__));
+    require_once($projectDir . '/lib/maxAPI/autoload.php');
+
+    $client = new maxCacheHtmlClient(array(
+        // Код автосалона
+        'dealer_id' => 5,
+
+        // Пароль для доступа к данным
+        'password' => 555,
+
+        // Номер используемой версии API
+        'api_version' => 1,
+
+        // Путь к каталогу, содержащему XSLT. Путь должен заканчиваться слэшем.
+        'xslt_dir' => $projectDir.'/lib/xslt/',
+
+        /* Путь к каталогу для кэширования. У процесса, выполняющего
+        скрипт должны быть права rwx на каталог и его файлы.
+        Путь должен заканчиваться слэшем */
+        'cache_dir' => $projectDir.'/cache/',
+
+        // Удалим кэширование данных в формате HTML на период отладки
+        'cached_html_themes' => array(),
+    ));
+
+    // Явно задаем тип запрашиваемых данных
+    $client->setRequestThemeName('vehicles');
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ru">
@@ -7,10 +36,13 @@
         <link rel="stylesheet" href="/css/blueprint/screen.css" type="text/css" media="screen,projection" />
         <link rel="stylesheet" href="/css/blueprint/print.css" type="text/css" media="print" />
         <!--[if IE]><link rel="stylesheet" href="/css/blueprint/ie.css" type="text/css" media="screen, projection" /><![endif]-->
+        <link rel="stylesheet" href="/css/usedcars.css" type="text/css" media="screen, projection" />
     </head>
     <body>
         <div class="container">
-            <h1>Работает!</h1>
+            <div class="span-16">
+                <?php echo $client->getHtml(); ?>
+            </div>
         </div>
     </body>
 </html>
