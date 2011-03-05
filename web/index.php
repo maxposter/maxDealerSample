a1b49766b483e373042d8fdd750e2533183783ce
Svel <Svel.Sontz@gmail.com>
2011-03-05 12:57:21 +0300

    Шаг 3: скрипт index.php, заглушка

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="ru">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <!-- Framework CSS -->
+        <link rel="stylesheet" href="/css/blueprint/screen.css" type="text/css" media="screen,projection" />
+        <link rel="stylesheet" href="/css/blueprint/print.css" type="text/css" media="print" />
+        <!--[if IE]><link rel="stylesheet" href="/css/blueprint/ie.css" type="text/css" media="screen, projection" /><![endif]-->
+    </head>
+    <body>
+        <div class="container">
+            <h1>Работает!</h1>
+        </div>
+    </body>
+</html>
