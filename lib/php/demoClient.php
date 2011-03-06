467dd58ca278971e3b0fcd448251ea1ffb4de222
Svel <Svel.Sontz@gmail.com>
2011-03-07 00:32:56 +0300

    Шаг 5: метод, отбрасывающий GET-параметры

--- lib/php/demoClient.php
+++ lib/php/demoClient.php
@@ -1,4 +1,18 @@
 <?php
 class demoClient extends maxCacheHtmlClient
 {
+    /**
+     * Удаление из URI GET-параметров запроса
+     *
+     * @return string
+     */
+    protected function getRequestURI()
+    {
+        $questionMarkPosition = strpos($_SERVER['REQUEST_URI'], '?');
+
+        return false !== $questionMarkPosition
+            ? substr($_SERVER['REQUEST_URI'], 0, $questionMarkPosition)
+            : $_SERVER['REQUEST_URI']
+            ;
+    }
 }
