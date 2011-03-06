56ccc84abae58f3b7cd0a61f7c53f942a8c8c998
Svel <Svel.Sontz@gmail.com>
2011-03-07 00:34:13 +0300

    Шаг 5: метод, определяющий тип запрашиваемых данных

--- lib/php/demoClient.php
+++ lib/php/demoClient.php
@@ -15,4 +15,27 @@ class demoClient extends maxCacheHtmlClient
             : $_SERVER['REQUEST_URI']
             ;
     }
+
+
+    /**
+     * Определение по URI темы запроса и парметров
+     *
+     * @return this
+     */
+    public function setRequest()
+    {
+        $requestURI = $this->getRequestURI();
+        // Если список авто
+        if (preg_match('/^\/$/', $requestURI)) {
+            // Устанавливаем тему запроса vehicles
+            $this->setRequestThemeName('vehicles');
+        }
+        // Если описание авто
+        elseif (preg_match('/^\/([0-9]{1,6})$/', $requestURI, $params)) {
+            // Устанавливаем тему запроса равную коду объявления
+            $this->setRequestThemeName($params[1]);
+        }
+
+        return $this;
+    }
 }
