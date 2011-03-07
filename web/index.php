c89e0af0339838c869c76783a0e65f4e9d675d44
Svel <Svel.Sontz@gmail.com>
2011-03-07 05:35:01 +0300

    Шаг 9: в скрипте index.php добавить загрузку XML не смотря на кэширование HTML

--- web/index.php
+++ web/index.php
@@ -26,6 +26,7 @@
 
     $client = demoClient::createInstance();
     $client->setRequest();
+    $client->getXml();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
